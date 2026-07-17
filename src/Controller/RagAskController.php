1bbdadac37817084520460171514abd163b01fd3
Ramtane Hidja <ramtane.hidja@smile.fr>
2026-07-17 12:19:57 +0200

    Fix type-hint for serializer->normalize() call
    SerializerInterface only declares serialize()/deserialize();
    normalize() belongs to NormalizerInterface, which the concrete
    serializer service also implements. Use an intersection type so
    static analysis (and the IDE) recognizes the call as valid.

--- src/Controller/RagAskController.php
+++ src/Controller/RagAskController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
@@ -16,7 +17,7 @@ final class RagAskController
 {
     public function __construct(
         private readonly RagQueryService $ragQueryService,
-        private readonly SerializerInterface $serializer,
+        private readonly SerializerInterface&NormalizerInterface $serializer,
         private readonly ValidatorInterface $validator,
     ) {
     }
